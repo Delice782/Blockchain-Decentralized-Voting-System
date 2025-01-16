f88ebcb49660a1e9befccb00f5031f2f0c2e29e6
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-01-16 23:31:50 +0000

    Update 1_deploy_contract.js

--- 1_deploy_contract.js
+++ 1_deploy_contract.js
@@ -1,4 +1,4 @@
-                  const Voting = artifacts.require("Voting");
+const Voting = artifacts.require("Voting");
 
 module.exports = function (deployer) {
   deployer.deploy(Voting, "Election 2025");
